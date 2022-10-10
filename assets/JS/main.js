836c6aaff1266cf1a91c99d77120c690b2c117d3
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-10 15:28:16 +0200

    indent

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,12 +1,12 @@
-/* Consegna:
+/*Consegna:
+
 Scrivi un programma che stampi in console i numeri da 1 a 100,
 ma che per i multipli di 3 stampi “Fizz” al posto del numero
 e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Come faccio a sapere se un numero è divisibile per un altro?
-Abbiamo visto qualcosa di particolare che possiamo usare?
+
 Consigli del giorno:
+
 Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 La condizione non funziona? controlliamo il loro ordine.
@@ -25,6 +25,6 @@ if ((i % 3) == 0) { /* check per divisibilità per 3 */
 } else if ((i % 5) == 0) { /* check per divisibilità per 5 */
     
 } else { /* se non é multiplo di 3 o di 5 rimane numero normale */
-    
+
 }
 }
