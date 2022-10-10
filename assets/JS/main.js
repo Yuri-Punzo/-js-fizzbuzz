1ab15af2fd6e4479d54ea9ac5a1f5de0d516a488
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-10 19:46:28 +0200

    rifatto tutto una seconda volta con bonus 1 e 2

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -21,22 +21,47 @@ const ul = document.querySelector('ul.list');
 for (let i = 1; i <= 100; i++) {   /* scrivo i 100 numeri */
     const li = document.createElement('li');
     if (((i % 3) == 0) && ((i % 5) == 0)) {           /* MINIMO COMUNE MULTIPLO 3 e 5 va messo in cima o poi non viene considerato perché avrebbe già assegnato i Fizz o i Buzz sopra ai numeri FizzBuzz*/
-        li.append("FizzBuzz");
         ul.append(li);
+        li.append("FizzBuzz");
         console.log("FizzBuzz");
     } else if ((i % 3) == 0) {     /* check per divisibilità per 3 */
-        li.append("Fizz");
         ul.append(li);
+        li.append("Fizz");
         console.log("Fizz");
     } else if ((i % 5) == 0) {     /* check per divisibilità per 5 */
-        li.append("Buzz");
         ul.append(li);
+        li.append("Buzz");
         console.log("Buzz");
     } else {                       /* se non é multiplo di 3 o di 5 rimane numero normale */
-        li.append(i);
         ul.append(li);
+        li.append(i);
         console.log(i);
     }
 
 }
 
+let x = document.querySelector(".container_2")
+
+for (let i = 1; i <= 100; i++) {
+    let y = document.createElement("div")
+    
+    if (((i % 3) == 0) && ((i % 5) == 0)) {
+        y.className = "square bg-danger d-flex align-items-center justify-content-center m-2"
+        x.append(y)
+        y.append("FizzBuzz")
+    } else if ((i % 3) == 0) {
+        y.className = "square bg-success d-flex align-items-center justify-content-center m-2"
+        x.append(y)
+        y.append("Fizz")
+    } else if ((i % 5) == 0) {
+        y.className = "square bg-secondary d-flex align-items-center justify-content-center m-2"
+        x.append(y)
+        y.append("Buzz")
+    } else {
+        y.className = "square bg-primary d-flex align-items-center justify-content-center m-2"
+        x.append(y)
+        y.append(i)
+    }
+
+
+}
